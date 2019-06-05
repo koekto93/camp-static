babe1ca89de812f87e70b091473d90c09d3d04d4
Mark <koekto93@mail.ru>
2019-06-05 20:35:31 +0300

    vertical news

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,106 @@
-/* import jQuery from "jquery";
-import popper from "popper.js";
-import bootstrap from "bootstrap";
-
-jQuery(function() {
-  jQuery("body").css("color", "blue");
-}); */
-
-// Render main template to the DOM
-// document.body.innerHTML = _.template(mainTemplate, model);
-import _ from "lodash";
+import _ from 'lodash';
 
 const captionWithIcon = _.template(
-  require("./../html/includes/caption-with-icon.html").default
+  require('./../html/includes/caption-with-icon.html').default,
 );
 
 const newsItem = _.template(
-  require("./../html/includes/news-item.html").default
+  require('./../html/includes/news-item.html').default,
 );
 
-const firstPageData = {
-  title: "Заголовок | Проект",
-  author: "Harrix",
+const newsBlock = _.template(
+  require('./../html/includes/news-block.html').default,
+);
+
+const verticalNewsBlock = _.template(
+  require('./../html/includes/vertical-news-block.html').default,
+);
+
+//Вставка блока с новостями
+const newsBlockData = {
+  className: '_with-caption', //для главного блока
+  blockClassName: '_row _j-between', //для блока news__block
   //caption
-  captionWithIcon: captionWithIcon,
+  caption: true,
+  captionWithIcon,
   captionWithIconData: {
-    className: "_row _a-center",
-    width: "23px",
-    height: "15px",
-    imgName: "news",
-    text: "Важная информация"
+    className: '_row _a-center',
+    width: '23px',
+    height: '15px',
+    imgName: 'news',
+    text: 'Важная информация',
   },
   //newsItem
-  newsItem: newsItem,
-  newsItemData: {
-    className: "_row",
-    date: "01.02.2019",
-    time: "14:10",
-    text: "Результаты распределения квот на отдых 2019"
-  }
+  newsItem,
+  newsItemData: [
+    {
+      className: '_row',
+      date: '01.02.2019',
+      time: '14:10',
+      text: 'Результаты распределения квот на отдых 2019',
+    },
+    {
+      className: '_row',
+      date: '01.02.2019',
+      time: '14:10',
+      text: 'Результаты распределения квот на отдых 2019',
+    },
+    {
+      className: '_row',
+      date: '01.02.2019',
+      time: '14:10',
+      text: 'Результаты распределения квот на отдых 2019',
+    },
+  ],
 };
-const text = _.template(require("./../html/includes/text.html").default);
 
-const content = document.body.querySelector(".content-container");
+const newsBlockContent = newsBlock(newsBlockData);
 
-content.innerHTML = text(firstPageData);
+//Вставка блока с вертикальными новостями и картинкой
+const verticalNewsBlockData = {
+  className: '_with-caption', //для главного блока
+  blockClassName: '_row _j-between', //для блока news__block
+  //img
+  img: true,
+  //caption
+  caption: true,
+  captionWithIcon,
+  captionWithIconData: {
+    className: '_row _a-center',
+    width: '23px',
+    height: '15px',
+    imgName: 'news',
+    text: 'Важная информация',
+  },
+  //newsItem
+  newsItem,
+  newsItemData: [
+    {
+      className: '_row',
+      date: '01.02.2019',
+      time: '14:10',
+      text: 'Результаты распределения квот на отдых 2019',
+    },
+    {
+      className: '_row',
+      date: '01.02.2019',
+      time: '14:10',
+      text: 'Результаты распределения квот на отдых 2019',
+    },
+    {
+      className: '_row',
+      date: '01.02.2019',
+      time: '14:10',
+      text: 'Результаты распределения квот на отдых 2019',
+    },
+  ],
+};
+
+const verticalNewsBlockContent = verticalNewsBlock(verticalNewsBlockData);
+
+//результат
+const result = newsBlockContent + verticalNewsBlockContent;
+
+const content = document.body.querySelector('.content-container');
+
+content.innerHTML = result;
